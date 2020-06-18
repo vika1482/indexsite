8d881e07a506bfdef10d4aeeaa3910e7d9b1ebe3
vika1482 <67109333+vika1482@users.noreply.github.com>
2020-06-18 21:18:02 +0700

    indexsite

---
+++ Новая папка/filter.js
@@ -0,0 +1,7 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myfilter() {
+    document.getElementById("myFilterdown").classList.toggle("showfilter");
+}
+
+// Close the dropdown menu if the user clicks outside of it
